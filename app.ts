1e06bb3996d0fdb896fcc86b30a09854231a906b
yunshian <yunshian.shiao@senao.com.tw>
2018-01-10 09:10:45 +0800

    for 迴圈大量生產產品

--- app.ts
+++ app.ts
@@ -4,12 +4,16 @@ import { NBBuilder } from './NBBuilder';
 import { Director } from './Director';
 
 let BuilderPC = new ComputerBuilder();
-let BuilderNB = new NBBuilder();
-
 let directorPC = new Director(BuilderPC);
+console.log('建立五台 PC');
+for (let index = 0; index < 5; index++) {
+  let PC = BuilderPC.GetResult();
+}
 
-let PC = BuilderPC.GetResult();
-
+let BuilderNB = new NBBuilder();
 let directorNB = new Director(BuilderNB);
+console.log('建立十台筆電');
+for (let index = 0; index < 10; index++) {
+  let NB = BuilderNB.GetResult();
+}
 
-let NB = BuilderNB.GetResult();
